009510c8bca844fd4c826708ab008ba07e0c96e9
Juliana Morikoshi <julianamorikoshi@gmail.com>
2020-12-15 23:51:58 -0300

    :package: Class-transform for controller response

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
@@ -9,14 +10,11 @@ export default class SessionsController {
 
     const authenticateUser = container.resolve(AuthenticateUserService);
 
-    const {
-      user: { password: authPassword, ...responseUser },
-      token,
-    } = await authenticateUser.execute({
+    const { user, token } = await authenticateUser.execute({
       email,
       password,
     });
 
-    return response.json({ user: responseUser, token });
+    return response.json({ user: classToClass(user), token });
   }
 }
